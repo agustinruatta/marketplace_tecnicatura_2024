5e86cfbc8f3899b05d43661301cd80b8b6dd6c85
Agustín Ruatta <agustinruatta@gmail.com>
2024-04-22 22:41:32 -0300

    Mostrar notebooks en página principal desde la API

--- index.js
+++ index.js
@@ -6,4 +6,29 @@ async function mostrarCantidadArticulos() {
     document.getElementById('cantidad-articulos').textContent = cantidadArticulos;
 }
 
+async function mostrarArticulos() {
+    const response = await fetch('https://my-json-server.typicode.com/agustinruatta/fake_json_server_db/products/');
+    const datos = await response.json();
+
+    for (const notebook of datos) {
+        const imagenNotebook = document.createElement('img');
+        imagenNotebook.src = notebook.image_url;
+
+        const tituloNotebook = document.createElement('strong');
+        tituloNotebook.textContent = notebook.title;
+
+        const link = document.createElement('a');
+        link.href = 'notebook.html';
+        link.appendChild(imagenNotebook);
+        link.appendChild(tituloNotebook);
+
+        const divContenedor = document.createElement('div');
+        divContenedor.className = 'preview-producto';
+        divContenedor.appendChild(link);
+
+        document.getElementById('listado-productos').appendChild(divContenedor);
+    }
+}
+
 mostrarCantidadArticulos();
+mostrarArticulos();
