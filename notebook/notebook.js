686e1b6d0f851ca5523e9cc6d62ba4ac58aaf035
Agustín Ruatta <agustinruatta@gmail.com>
2024-05-20 22:42:28 -0300

    Agregado posibilidad de dar feedback

--- notebook/notebook.js
+++ notebook/notebook.js
@@ -2,16 +2,6 @@ const app = Vue.createApp({
     data() {
         return {
             carroDeCompras: [],
-            feedbacks: [
-                {
-                    comment: 'Excelente producto',
-                    score: 5
-                },
-                {
-                    comment: 'Bastante bueno',
-                    score: 4
-                }
-            ]
         }
     },
     methods: {
