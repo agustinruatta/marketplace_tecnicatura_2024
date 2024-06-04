cd4d8e75118252d8b7681999f1a9df5ff488818d
Agustín Ruatta <agustinruatta@gmail.com>
2024-06-03 21:30:57 -0300

    Agregado Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">Notebook</router-link>
+    <router-link to="/notebook">Notebook</router-link>
   </nav>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import NotebookInfo from '@/views/NotebookView.vue';
 
 const routes = [
   {
@@ -8,12 +9,12 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/notebook',
+    name: 'NotebookInfo',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/NotebookView.vue')
+    component: NotebookInfo
   }
 ]
 
